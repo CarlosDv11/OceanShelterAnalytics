39d1043e61c4f2d492d10c1ee12d79f9e82027f0
Carlos <carloselima2001@gmail.com>
2024-12-05 00:46:47 -0300

    add vercel.json

--- Login/login.js
+++ Login/login.js
@@ -1,10 +1,12 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     if (username === "admin" && password === "1234") {
+        // Salvar estado de autenticação
+        localStorage.setItem("isAuthenticated", "true");
         window.location.href = "../DashBoard/home.html";
     } else {
         document.getElementById("error-message").textContent = "Usuário ou senha incorretos!";
